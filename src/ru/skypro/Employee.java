8bcb32cd9704ba357b4289fc261b906d0d862875
alekdersan <repka1795@gmail.com>
2022-02-03 00:48:00 +0300

    Сделана Курсовая работа - базовая сложность.

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,62 @@
+package ru.skypro;
+
+import java.util.Objects;
+
+public class Employee {
+    private final String fullName;
+    private String department;
+    private int salary;
+    private static int counter = 1;
+    private int id;
+
+    public Employee(String fullName, String department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment() {
+        this.department = department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary() {
+        this.salary = salary;
+    }
+
+    public int getId() { return id; }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Float.compare(employee.salary, salary) == 0 && Objects.equals(fullName, employee.fullName) && Objects.equals(department, employee.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary);
+    }
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,86 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Employee[] employeesArray = new Employee[10];
+        employeesArray[0] = new Employee("Плотников Андрей Михайлович", "Отдел кадров", 45_000);
+        employeesArray[1] = new Employee("Кузнецова Анна Евгеньевна", "Отдел снабжения", 48_000);
+        employeesArray[2] = new Employee("Лаврова Анна Михайловна", "Юридическиий отдел", 65_000);
+        employeesArray[3] = new Employee("Мельникова Софья Арсентьевна", "Финансовая служба", 56_000);
+        employeesArray[4] = new Employee("Золотарева Варвара Тимуровна", "Финансовая служба", 35_000);
+        employeesArray[5] = new Employee("Миронов Иван Евгеньевич", "Информационный отдел", 37_000);
+        employeesArray[6] = new Employee("Андреева Анастасия Глебовна", "Отдел снабжения", 32_000);
+        employeesArray[7] = new Employee("Белов Макар Даниилович", "Отдел кадров", 27_000);
+        employeesArray[8] = new Employee("Морозова Евдокия Ярославовна", "Финансовая служба", 41_000);
+        employeesArray[9] = new Employee("Семенова Варвара Игоревна", "Информационный отдел", 30_000);
+
+        getAllEmployees(employeesArray);
+        System.out.println();
+
+        spendMonthlyOnSalary(employeesArray);
+        System.out.println();
+
+        findMaxSalary(employeesArray);
+        System.out.println();
+
+        findMinSalary(employeesArray);
+        System.out.println();
+
+        findAverageSalary(employeesArray);
+        System.out.println();
+
+        fullNameEmployees(employeesArray);
+    }
+
+    public static void getAllEmployees(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+        }
+    }
+
+    public static void spendMonthlyOnSalary(Employee[] employees) {
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalary();
+        }
+        System.out.println("Расходы на зарплаты в месяц = " + sum);
+    }
+
+    public static void findMinSalary(Employee[] employees) {
+        float minSalary = 100_000;
+        for (Employee employee : employees) {
+            if (employee.getSalary() < minSalary) {
+                minSalary = employee.getSalary();
+            }
+        }
+        System.out.println("Минимальная зарплата в коллективе = " + minSalary);
+    }
+
+    public static void findMaxSalary(Employee[] employees) {
+        float maxSalary = 0;
+        for (Employee employee : employees) {
+            if (employee.getSalary() > maxSalary) {
+                maxSalary = employee.getSalary();
+            }
+        }
+        System.out.println("Максимальная зарплата в коллективе = " + maxSalary);
+    }
+
+    public static void findAverageSalary(Employee[] employees) {
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalary();
+        }
+        float averageSalary = sum / employees.length;
+        System.out.println("Средняя зарплата в коллективе = " + averageSalary);
+    }
+
+    public static void fullNameEmployees(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println("ФИО сотрудника: " + employees[i].getFullName());
+        }
     }
 }
